1cd2a9d0fdab925f9cde3f23006e4242e83a47de
Alban Linard <alban@linard.fr>
2015-04-26 23:51:15 +0200

    Remove count in http server example.

--- test/server-http.lua
+++ test/server-http.lua
@@ -4,6 +4,10 @@ local copev  = require "copas.ev"
 copev.compatibility = false
 
 local server = socket.bind ("127.0.0.1", 8080)
+server:setoption ("keepalive", true)
+server:setoption ("reuseaddr", true)
+server:setoption ("tcp-nodelay", true)
+
 --local profiler = require "profiler"
 --profiler:start "data.out"
 
@@ -16,16 +20,25 @@ Content-Length: 14
 Hello, World!
 ]]
 
+--local count = 0
 copev.addserver (server, function (skt)
+--  if count == 10000 then
+--    copev.removeserver (server)
+--  end
   while true do
     local message = skt:receive "*l"
     if message == nil then
       return
     elseif message == "" then
+--      count = count+1
       skt:send (answer)
       return
     end
   end
 end)
 
-copev.loop ()
+--ProFi = require "ProFi"
+--ProFi:start ()
+copev.loop  ()
+--ProFi:stop  ()
+--ProFi:writeReport "report2.txt"
